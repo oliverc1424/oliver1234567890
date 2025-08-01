264098867cd968acaa6c6240ae4aed98069f8306
Student <47796382@qq.com>
2025-08-01 09:42:16 -0700

    add labelytdhrfyutuf

--- main.py
+++ main.py
@@ -10,14 +10,18 @@ parser = argparse.ArgumentParser()
 parser.add_argument("filename", type=str, help="filename of the image to process")
 parser.add_argument("--network", type=str, default="googlenet", help="model to use, can be:  googlenet, resnet-18, ect. (see --help for others)")
 opt = parser.parse_args()
-
 img = jetson_utils.loadImage(opt.filename)
-
-net = jetson_inference.imageNet(opt.network)
+net = jetson_inference.imageNet(
+    opt.network,
+    model="./resent18.onnx",
+    labels="./dataset/labels.txt",
+    input_blob="input_0",
+    output_blob="output_0",
+)
 
 class_idx, confidence = net.Classify(img)
 
 class_desc = net.GetClassDesc(class_idx)
 
 print("image is recognized as "+ str(class_desc) +" (class #"+ str(class_idx) +") with " + str(confidence*100)+"% confidence")
-Recap
+
